05eb78af49607219de50e0eb0745db336ba05d95
Rafalcho <raf.cholaj@gmail.com>
2017-06-07 20:10:48 +0200

    fetching articles work

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import {getNews} from '../utils/api';
 
 class ArticleList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: null,
+    };
+  }
+
+  componentDidMount() {
+    getNews()
+      .then(data => {
+        this.setState({
+          articles: data,
+        });
+      });
+  }
   render() {
     return <div className='article-list'>
-      Articles
+      <ul>
+        {this.state.articles ? this.state.articles.map(article => {
+          return <li>{article.title}</li>;
+        }) :  null}
+      </ul>
+
     </div>;
   }
 }
---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+export const getNews = () => {
+  const query = `{articles(t: [Article], service: [Wiadomosci]) {id title body{data}img{url}}}`;
+
+  const url = `https://mobileapi.wp.pl/v1/graphql?query=${query}`;
+
+  const fetchNews = fetch(url);
+
+  return fetchNews.then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Fetching news failed');
+    }
+  })
+  .then(response => {
+    console.log(response.data.articles);
+    return response.data.articles;
+
+  })
+  .catch(error => {
+    console.log('Fetching data error:', error);
+  });
+};
