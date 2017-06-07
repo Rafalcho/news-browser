5fc042080ba3039c40bca807f22267f863d6ab5b
Rafalcho <raf.cholaj@gmail.com>
2017-06-07 18:49:08 +0200

    basic article list component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import ArticleList from './components/ArticleList';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-    
+        <ArticleList />
       </div>
     );
   }
---
+++ src/components/ArticleList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ArticleList extends Component {
+  render() {
+    return <div className='article-list'>
+      Articles
+    </div>;
+  }
+}
+
+export default ArticleList;
