b959fffb0b60496456583cdaa1133b28346fa60e
Rafalcho <raf.cholaj@gmail.com>
2017-06-08 12:23:46 +0200

    basic routing added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import ArticleList from './components/ArticleList';
+import Article from './components/Article';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+    <Router>
       <div className='App'>
-        <ArticleList />
+        <Switch>
+          <Route exact path='/' component={ArticleList} />
+          <Route path='/article' component={Article} />
+          <Route render={() => <p>Not Found</p>} />
+        </Switch>
       </div>
+    </Router>
     );
   }
 }
