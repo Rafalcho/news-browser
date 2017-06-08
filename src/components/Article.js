e2aa89ff27b1d65be384ddb73091656abd438956
Rafalcho <raf.cholaj@gmail.com>
2017-06-08 15:37:52 +0200

    article component

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react';
+import {getArticle} from '../utils/api';
 
 class Article extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articleBody: null,
+    };
+  }
+
+  componentDidMount() {
+    getArticle('http://wiadomosci.wp.pl/posel-kukiz15-chce-zamykac-sedziow-tk-w-obozach-tylko-sobie-zartowalem-6131385401800833a')
+      .then(data => {
+        this.setState({
+          articleBody: data
+        });
+      });
+  }
+
   render() {
-    return <div></div>;
+    let article;
+    let title;
+    let image;
+
+    if (this.state.articleBody) {
+      title = this.state.articleBody.title;
+      image = this.state.articleBody.img.url || null;
+      article = this.state.articleBody.body.map((part, index) => {
+        function createMarkup() {
+          return {__html: part.data};
+        }
+        return <div
+          key={index}
+          dangerouslySetInnerHTML={createMarkup()} />;
+      });
+    }
+    return <div className='article'>
+      <h1>{title}</h1>
+      <img
+        className='article-photo'
+        src={image}
+        alt='article'/>
+      {article}
+    </div>;
   }
 }
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -21,3 +21,25 @@ export const getNews = () => {
     console.log('Fetching data error:', error);
   });
 };
+
+export const getArticle = (articleUrl) => {
+  const query = `{article(url:"${articleUrl}") {body {data},title,img {url},}}`;
+  const url = `https://mobileapi.wp.pl/v1/graphql?query=${query}`;
+
+  const fetchArticle = fetch(url);
+  return fetchArticle.then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Fetching article failed');
+    }
+  })
+  .then(response => {
+    console.log(response.data.article);
+    return response.data.article;
+
+  })
+  .catch(error => {
+    console.log('Fetching data error:', error);
+  });
+};
